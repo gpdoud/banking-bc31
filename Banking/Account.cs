f3ed1796acbf64232f8e9169c5522b20879d6eb9
Greg Doud <gpdoud@gmail.com>
2021-02-15 16:34:07 -0500

    "initial commit"

---
+++ Banking/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking {
+
+    class Account {
+
+        public string AccountNumber { get; set; }
+        public string RoutingNumber { get; set; }
+        public string Description { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; } = 0;
+
+        public void Deposit(decimal amount) {
+            if(amount > 0) {
+                Balance += amount;
+
+            }
+        }
+        public void Withdraw(decimal amount) {
+            if(amount < 0) {
+                return;
+            }
+            if(Balance >= amount) {
+                Balance -= amount;
+            } else {
+                Console.WriteLine($"Insufficient funds!");
+            }
+        }
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Banking {
+    class Program {
+        static void Main(string[] args) {
+
+            var acct1 = new Account();
+            acct1.AccountNumber = "100";
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+            acct1.Deposit(200);
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+            acct1.Withdraw(30);
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+            acct1.Withdraw(230);
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+            acct1.Deposit(-200);
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+            acct1.Withdraw(-200);
+            Console.WriteLine($"Account {acct1.AccountNumber} balance is {acct1.Balance}");
+        }
+    }
+}
